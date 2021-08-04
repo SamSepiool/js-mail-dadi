fd0ce3d78682320f20e2f97094eeeb8ee83200c3
SamSepiool <simone.severini@outlook.it>
2021-08-04 14:06:04 +0200

    add dadi section

--- js/script.js
+++ js/script.js
@@ -21,6 +21,30 @@ function whitelist() {
     }
 }
 
+// DADI
+// 1)Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// 2)Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+
+function userInput(){
+    // 1)
+    dadoUser = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('utente').innerHTML += dadoUser;
+
+    dadoCpu = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('cpu').innerHTML += dadoCpu;
+
+    // 3)
+    if (dadoUser > dadoCpu){
+        document.getElementById('risultato').innerHTML = 'Hai vinto!';
+
+    } else if (dadoUser < dadoCpu){
+        document.getElementById('risultato').innerHTML = 'Hai perso...';
+
+    } else {
+        document.getElementById('risultato').innerHTML = 'Pareggio';
+    }
+
+}
 
 
